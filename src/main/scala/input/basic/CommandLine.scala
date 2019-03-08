a17d8baac4917db6f4b3820ec0171fe67476dbe9
Sirealism <m4popesc@ryerson.ca>
2019-03-08 11:40:49 -0500

    Removing useless imports

--- src/main/scala/input/basic/CommandLine.scala
+++ src/main/scala/input/basic/CommandLine.scala
@@ -2,9 +2,6 @@ package input.basic
 
 import context.Process
 
-import scala.io._
-import scala.util.{Failure, Success}
-
 object CommandLine extends App {
   new StringInput(new Process[String, String]).process(java.lang.System.in, Console.out)
 }
